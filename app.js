f9710843bfcbab4cab8771dd5babe43c8a90f329
JoseSachango <62193290+JoseSachango@users.noreply.github.com>
2020-12-02 22:21:53 -0600

    Was able to render content onto an html file

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-let EmployeeObjects =[]
+var EmployeeObjects =[]
+
 function EngineerPrompt(){
     inquirer.prompt([
         //Engineer prompts
@@ -47,24 +48,29 @@ function EngineerPrompt(){
         }
 
     ]).then((response)=>{
-        console.log(response)
+        
 
             if(response.addEmployees==="I don't wnat to add any more team members."){
+
+                var renderHtml = render(EmployeeObjects);
+                fs.writeFile(outputPath,renderHtml,(err)=>console.log(err));
+                console.log(EmployeeObjects);
+
                 return 
             }
             else if(response.addEmployees==="Engineer"){
-                console.log(response.addEmployees);
+                
                 EngineerPrompt();
                 
             }
             else if(response.addEmployees==="Intern"){
-                console.log(response.addEmployees);
+               
                 InternPrompt();
             }
         
         var EngineerObject = new Engineer(response.Name,response.id,response.email,response.gitHub)
 
-        console.log(EngineerObject)
+        
 
         EmployeeObjects.push(EngineerObject)
     })
@@ -104,23 +110,27 @@ function InternPrompt(){
         }
 
     ]).then((response)=>{
-        console.log(response)
-
+        
             if(response.addEmployees==="I don't wnat to add any more team members."){
+
+                var renderHtml = render(EmployeeObjects);
+                fs.writeFile(outputPath,renderHtml,(err)=>console.log(err));
+                console.log(EmployeeObjects);
+
                 return 
             }
             else if(response.addEmployees==="Engineer"){
-                console.log(response.addEmployees);
+               
                 EngineerPrompt();
                 
             }
             else if(response.addEmployees==="Intern"){
-                console.log(response.addEmployees);
+                
                 InternPrompt();
             }
         
         var InternObject = new Intern(response.Name,response.id,response.email,response.school)
-            console.log(InternObject)
+          
         EmployeeObjects.push(InternObject)
     })
 }
@@ -161,36 +171,52 @@ function ManagerPrompt(){
 
 
         ]).then((response)=>{
-            console.log(response)
+          
+                var ManagerObject = new Manager(response.Name,response.id,response.email,response.officeNumber)
+                
+
+                EmployeeObjects.push(ManagerObject);
+
 
                 if(response.addEmployees==="I don't wnat to add any more team members."){
+
+                    var renderHtml = render(EmployeeObjects);
+                    fs.writeFile(outputPath,renderHtml,(err)=>console.log(err));
+                    console.log(EmployeeObjects);
+                    
+                    
                     return 
                 }
                 else if(response.addEmployees==="Engineer"){
-                    console.log(response.addEmployees);
+                    
                     EngineerPrompt();
                     
                 }
                 else if(response.addEmployees==="Intern"){
-                    console.log(response.addEmployees);
+                    
                     InternPrompt();
                 }
 
-            var ManagerObject = new Manager(response.Name,response.id,response.email,response.officeNumber)
-            console.log(ManagerObject)
-
-            EmployeeObjects.push(ManagerObject);
+         
+            
         })
 
 }
 
+
+
+
 ManagerPrompt()
+
+
 
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-render(EmployeeObjects);
+
+
+
 
 
 
@@ -199,6 +225,10 @@ render(EmployeeObjects);
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
+
+
+
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
